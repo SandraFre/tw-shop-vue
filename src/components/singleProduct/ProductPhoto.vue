d44dfe7b1cc3320ecf776d3a53e497bf04e56bdf
SandraFre <SandraFre@users.noreply.github.com>
2020-07-03 21:05:40 +0300

    SingleProduct grid

---
+++ src/components/singleProduct/ProductPhoto.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="grid__photo">
+photo
+  </div>
+</template>
+
+<script>
+name: "product-photo"
+</script>
+
+<style lang="scss" scoped>
+    .grid__photo {
+        grid-area: photo;
+        background-color: $primary;
+    }
+</style>
--- src/views/SingleProduct.vue
+++ src/views/SingleProduct.vue
@@ -1,18 +1,66 @@
 <template>
-<div class="container container-main">
-  <div class="grid">
-
+  <div class="container container-main">
+    <div class="grid">
+      <product-photo />
+      <div class="grid__title">asadfd</div>
+      <div class="grid__dimensions">hgjkloi</div>
+      <div class="grid__code">4567</div>
+      <div class="grid__price">klo</div>
+      <div class="grid__order">rtoi90</div>
+      <div class="grid__similar">879865</div>
+      <div class="grid__recipies">we4r</div>
+    </div>
   </div>
-
-</div>
 </template>
 
 <script>
+import ProductPhoto from "@/components/singleProduct/ProductPhoto";
+
 export default {
-    name: "product"
-}
+  name: "product",
+  components: {
+    ProductPhoto
+  }
+};
 </script>
 
-<style>
-  
+<style lang="scss" scoped>
+.grid {
+  display: grid;
+  padding-top: 1.5rem;
+  grid-template:
+    "photo photo title title"
+    "photo photo dimensions dimensions"
+    "photo photo code code"
+    "photo photo price price"
+    "tocart tocart tocart tocart"
+    "similar similar similar similar"
+    "similar similar similar similar"
+    "recipies recipies recipies recipies"
+    "recipies recipies recipies recipies"
+    "recipies recipies recipies recipies";
+    
+  gap: 1.5rem;
+  &__title {
+    grid-area: title;
+  }
+  &__dimensions {
+    grid-area: dimensions;
+  }
+  &__code {
+    grid-area: code;
+  }
+  &__price {
+    grid-area: price;
+  }
+  &__order {
+    grid-area: tocart;
+  }
+  &__similar{
+    grid-area: similar;
+  }
+  &__recipies{
+    grid-area: recipies;
+  }
+}
 </style>
